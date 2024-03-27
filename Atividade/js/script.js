0e0e8f4a037f324e54905930e5459dd818d3b01b
Thiago Vanucci <vanucci.thiagocandarola@gmail.com>
2024-03-27 17:07:50 -0300

    Atividade 25 exercicios

---
+++ Atividade/js/script.js
@@ -0,0 +1,26 @@
+exercicio1=()=> {
+    document.getElementById("resposta").innerHTML = "<h2>Resposta da Atividade</h2>";
+    for (let i = 1; i <= 10; i++) {
+        // console.log(i);
+        document.getElementById("resposta").innerHTML += `<h2>${i}</h2>`
+        
+    }
+} 
+
+
+exercicio2=()=> {
+    document.getElementById("resposta").innerHTML = "<h2>Resposta da Atividade</h2>";
+    let num = document.getElementById("num").value
+    for (let i = 0; i <= num ; i+= 2) {
+        // console.log(i);
+        document.getElementById("resposta").innerHTML += `<h2>${i}</h2>`
+        
+
+    }
+}
+
+
+exercicio3=()=> {
+    if 
+        
+}
